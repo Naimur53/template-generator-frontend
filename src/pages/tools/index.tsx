1bfbb1386ed82830c16edec35fa0c4be49a07232
Naimur53 <naimurrhman53@gmail.com>
2023-09-26 14:08:27 +0600

    added file

---
+++ src/pages/tools/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface IToolsProps {
+}
+
+const Tools: React.FunctionComponent<IToolsProps> = (props) => {
+  return <div>
+    hi
+  </div>;
+};
+
+export default Tools;

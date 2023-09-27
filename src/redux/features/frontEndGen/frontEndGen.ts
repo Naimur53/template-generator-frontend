691d33c6616f7403b11545e89f7f8a4580166e5f
naimur53 <naimurrhman53@gmail.com>
2023-09-27 23:35:16 +0600

    remove comment

--- src/redux/features/frontEndGen/frontEndGen.ts
+++ src/redux/features/frontEndGen/frontEndGen.ts
@@ -55,7 +55,4 @@ export const {
   removePackage,
 } = frontEndGen.actions;
 
-// Other code such as selectors can use the imported `RootState` type
-// export const selectCount = (state: RootState) => state.counter.value;
-
 export default frontEndGen.reducer;
